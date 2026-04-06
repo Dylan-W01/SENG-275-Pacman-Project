4420a11246fcdd4f4146fbcc95b0345d40c3d364
Dylan Whitbread <dylanwhitbread@uvic.ca>
2026-04-05 23:45:06 -0700

    Continuing to work on integration testing

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,11 +1,18 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.board.Board;
 import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.board.Square;
+import nl.tudelft.jpacman.npc.Ghost;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import nl.tudelft.jpacman.level.MapParser;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,14 +34,40 @@ class MapParserTest {
     // Produce a map comprised of just one wall using Mockito
     @Test
     void testParseMap() {
-        char[][] map = new char[0][0];//should be a list of chars to make a wall
+        int height = 10;
+        int width = 10;
+        char[][] map = new char[width][height];//should be a list of chars to make a wall
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                map[x][y] = ' ';
+            }
+        }
+        map[4][4] = '#';
+
 //        mock(LevelFactory);
 //        MapParser(mock(LevelFactory.class), mock(BoardFactory.class).parseMap(map);
+        LevelFactory levelCreatorMock = mock(LevelFactory.class);
+        BoardFactory boardCreatorMock = mock(BoardFactory.class);
+
         MapParser mockMapParser = mock(MapParser.class);
+        MapParser mockMapParser2 = new MapParser(levelCreatorMock, boardCreatorMock);
 
         Level mockLevel = mockMapParser.parseMap(map);
         //verifying that parseMap() has been called
+        //promote dummy to spy
         verify(mockMapParser).parseMap(map);
+//        verify(mockMapParser2).parseMap(map);
+        //promote to a stub
+            //but this test does not create a map with a wall... it just checks to see that parseMap returns what it's supposed to.
+        when(mockMapParser.parseMap(map)).thenReturn(null);
+
+
+//        Square[][] grid = new Square[width][height];
+//        Board board = boardCreatorMock.createBoard(grid);
+//        List<Ghost> ghosts = new ArrayList<>();
+//        List<Square> startPositions = new ArrayList<>();
+//        when(mockMapParser.parseMap(map)).thenReturn(levelCreatorMock.createLevel(board, ghosts, startPositions));
+        //promote stub to a mock
 
 
 //        when(mockMapParser.parseMap())
