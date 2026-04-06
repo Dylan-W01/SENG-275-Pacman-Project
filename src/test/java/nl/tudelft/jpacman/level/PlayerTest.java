a70b1162e6d8786e401f84da26169ebf6735ed63
Rauan Kairatx <sengrauan@gmail.com>
2026-04-06 14:16:14 -0700

    Set up for Mutation Testing and added 1 test

--- src/test/java/nl/tudelft/jpacman/level/PlayerTest.java
+++ src/test/java/nl/tudelft/jpacman/level/PlayerTest.java
@@ -1,12 +1,31 @@
 package nl.tudelft.jpacman.level;
 
 import org.junit.jupiter.api.Test;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerFactory;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.junit.jupiter.api.BeforeEach;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    private Player player;
+
+    @BeforeEach
+    void setup() {
+        PacManSprites sprites = new PacManSprites();
+        PlayerFactory factory = new PlayerFactory(sprites);
+        player = factory.createPacMan();
+    }
+
 
     @Test
     void isAlive() {
+    }
+
+    @Test
+    void playerStartsAlive(){
+        assertThat(player.isAlive()).isTrue();
     }
 }
