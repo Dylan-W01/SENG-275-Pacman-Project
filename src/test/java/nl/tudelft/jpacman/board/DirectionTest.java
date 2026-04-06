7e9bd5bf551dd5ff3781dc91a5f19c7b9e55aaa7
Rauan Kairatx <sengrauan@gmail.com>
2026-04-06 13:16:53 -0700

    Added first property test, with all the set up for the next ones

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -3,6 +3,8 @@ package nl.tudelft.jpacman.board;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import net.jqwik.api.*;
+import net.jqwik.api.arbitraries.*;
 
 /**
  * A very simple (and not particularly useful)
@@ -18,5 +20,14 @@ public class DirectionTest {
     void testNorth() {
         Direction north = Direction.valueOf("NORTH");
         assertThat(north.getDeltaY()).isEqualTo(-1);
+    }
+
+    @Provide
+    public Arbitrary<Direction> directions(){
+        return Arbitraries.of(Direction.values());
+    }
+    @Property
+    void directions_X(@ForAll("directions") Direction dir){
+        assertThat(dir.getDeltaX()).isBetween(-1,1);
     }
 }
