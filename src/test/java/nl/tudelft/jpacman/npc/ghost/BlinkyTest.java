e51ad93ef986593b546df18ff4224bda56e959b1
Dylan Whitbread <dylanwhitbread@uvic.ca>
2026-04-06 22:53:16 -0700

    Finished one of my tests

--- src/test/java/nl/tudelft/jpacman/npc/ghost/BlinkyTest.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/BlinkyTest.java
@@ -1,11 +1,6 @@
 package nl.tudelft.jpacman.npc.ghost;
 
-import nl.tudelft.jpacman.board.Board;
-import nl.tudelft.jpacman.board.BoardFactory;
-import nl.tudelft.jpacman.board.Square;
-import nl.tudelft.jpacman.board.Unit;
-import nl.tudelft.jpacman.game.GameFactory;
-import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.board.*;
 import nl.tudelft.jpacman.level.LevelFactory;
 import nl.tudelft.jpacman.level.MapParser;
 import nl.tudelft.jpacman.npc.Ghost;
@@ -15,26 +10,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-
-import nl.tudelft.jpacman.npc.ghost.GhostMapParser;
-import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
-import java.io.InputStream;
+
 import java.io.IOException;
+import java.util.List;
 
 class BlinkyTest {
     //set up map with player and ghost
 
-    // test if horizontal distance (between G and P) is greater, G moves
-    // Given ghost G and player P are on a map
-    // When horizontal distance between G and P is greater than vertical distance between them
-    // then G moves towards P horizontally
+
 
     private MapParser parser;
 
@@ -43,21 +31,51 @@ class BlinkyTest {
      */
     @BeforeEach
     void setUp() {
+
         PacManSprites sprites = new PacManSprites();
         LevelFactory levelFactory = new LevelFactory(
                 sprites,
                 new GhostFactory(sprites),
                 mock(PointCalculator.class));
-        parser = new MapParser(levelFactory, new BoardFactory(sprites));
+//        parser = new MapParser(levelFactory, new BoardFactory(sprites));
+        parser = new GhostMapParser(levelFactory, new BoardFactory(sprites), new GhostFactory(sprites));
     }
 
     @Test
-    void testAiMoveHorizontal() {
-        try (InputStream i = getClass().getResourceAsStream("testboard.txt")) {
+    void testGhostOnMap() {
+        try (InputStream i = getClass().getResourceAsStream("/blinkytestboard.txt")) {
             Board b = parser.parseMap(i).getBoard();
             Square s1 = b.squareAt(1, 1);
+            Square[][] board = new Square[1][1];
+//            grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createClyde());
+//            parser.addSquare(board, new ArrayList<>(), new ArrayList<>(), 4, 3, 'B');
             Unit unit = Navigation.findNearest(Ghost.class, s1);
             assertThat(unit).isNotNull();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+        // test if horizontal distance (between G and P) is less, G moves
+        // Given ghost G and player P are on a map
+        // When horizontal distance between G and P is less than vertical distance between them
+        // then G moves towards P horizontally
+        // Given ghost G and player P are on a map
+        // When vertical  distance between G and P is less than horizontal distance between them
+        // then G moves towards P vertically
+
+        //if player starts at (1, 1) and ghost starts at (4, 2), ghost moves NORTH (closes shortest gap), then moves WEST (glosest next shortest gap)
+    void testnextAiMove() {
+
+
+        try (InputStream i = getClass().getResourceAsStream("/blinkytestboard.txt")) {
+            Board b = parser.parseMap(i).getBoard();
+            Square sqr_from = b.squareAt(4, 2);
+            Square sqr_to = b.squareAt(1, 1);
+            List<Direction> path = Navigation
+                    .shortestPath(sqr_from, sqr_to, mock(Unit.class));
+            assertThat(path).containsExactly(Direction.NORTH, Direction.WEST, Direction.WEST, Direction.WEST);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -65,6 +83,13 @@ class BlinkyTest {
 
 
     }
+    /**
+     * Tests if the algorithm can find a path in a straight line.
+     */
+//    @Test
+//    void testSimplePath() {
+//
+//    }
 
 
 
